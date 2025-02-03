b9977f74add77e008a5a4e2c8d5fb6c4d305e052
Melinay İdil DEVRİMCİ <middevrimci@gmail.com>
2025-02-03 23:44:28 +0300

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,41 @@
+import aiohttp
+import random
+
+class Pokemon:
+    pokemons = {}
+   
+    def __init__(self, pokemon_trainer):
+        self.pokemon_trainer = pokemon_trainer
+        self.pokemon_number = random.randint(1, 1000)
+        self.name = None
+        if pokemon_trainer not in Pokemon.pokemons:
+            Pokemon.pokemons[pokemon_trainer] = self
+        else:
+            self = Pokemon.pokemons[pokemon_trainer]
+
+    async def get_name(self):
+       
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  
+        async with aiohttp.ClientSession() as session:  
+            async with session.get(url) as response:  
+                if response.status == 200:
+                    data = await response.json()  
+                    return data['forms'][0]['name']  
+                else:
+                    return "Pikachu"  
+
+    async def info(self):
+        
+        if not self.name:
+            self.name = await self.get_name()  
+        return f"Pokémonunuzun ismi: {self.name}"  
+
+    async def show_img(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://pokeapi.co/api/v2/pokemon/9") as response:
+                if response.status == 200:
+                    data = await response.json()
+                    return data['sprites']['front_default']
+                else:
+                    return None
